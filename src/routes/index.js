787d5e57644040d73750f2ca894c22b37e917670
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-17 17:10:14 +0200

    promise all async await

--- src/routes/index.js
+++ src/routes/index.js
@@ -40,44 +40,36 @@ export async function post() {
 		price: Date.now()
 	};
 
-	try {
-		const createMessage = gql`
-			mutation createMessage($name: String!, $text: String!, $price: Int!) {
-				createMessage(data: { name: $name, text: $text, price: $price }) {
-					id
-					name
-					text
-					price
-				}
-			}
-		`;
-		const createdMessage = await client.request(createMessage, variables, requestHeaders);
-		// CATCH ERROR HERE !!
+	// const createMessage = gql`
+	// 	mutation createMessage($name: String!, $text: String!, $price: Int!) {
+	// 		createMessage(data: { name: $name, text: $text, price: $price }) {
+	// 			id
+	// 			name
+	// 			text
+	// 			price
+	// 		}
+	// 	}
+	// `;
+	// const createdMessage = await client.request(createMessage, variables, requestHeaders);
+	// // CATCH ERROR HERE !!
 
-		const publishMessage = gql`
-			mutation publishMessage($id: ID!) {
-				publishMessage(where: { id: $id }, to: PUBLISHED) {
-					id
-				}
-			}
-		`;
-		const messageID = { id: createdMessage.createMessage.id };
-		const publishedMessage = await client.request(publishMessage, messageID, requestHeaders);
-		// CATCH ERROR HERE !!
+	// const publishMessage = gql`
+	// 	mutation publishMessage($id: ID!) {
+	// 		publishMessage(where: { id: $id }, to: PUBLISHED) {
+	// 			id
+	// 		}
+	// 	}
+	// `;
+	// const messageID = { id: createdMessage.createMessage.id };
+	// const publishedMessage = await client.request(publishMessage, messageID, requestHeaders);
+	// // CATCH ERROR HERE !!
 
-		let returnedMessage = JSON.stringify(publishedMessage);
-		console.log(returnedMessage);
+	// let returnedMessage = JSON.stringify(publishedMessage);
+	// console.log(returnedMessage);
 
-		return {
-			status: 200
-		};
-	} catch (error) {
-		console.error(JSON.stringify(error, undefined, 2));
-		return {
-			status: 500,
-			body: {
-				error: 'Server error : ' + error
-			}
-		};
-	}
+	return {
+		status: 200
+	};
 }
+
+//  https://www.geeksforgeeks.org/explain-promise-all-with-async-await-in-javascript/
