109213bf0a589ecf30504d472333b0eefc2223d5
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-18 16:32:32 +0200

    graphql-request error handling

--- src/routes/api.json.js
+++ src/routes/api.json.js
@@ -3,38 +3,27 @@ import { gql } from 'graphql-request';
 
 export async function get() {
 	console.log('api.json');
-	try {
-		const getMessages = gql`
-			query getMessages {
-				messages(first: 1000) {
-					id
-					name
-					text
-					price
-				}
-			}
-		`;
-		// here messages HAS to be in an OBJECT and cannot later be put in an object when returned with the body
-		const { messages } = await client.request(getMessages);
-		// console.log(messages);
 
-		if (messages) {
-			return {
-				status: 200,
-				// mind the difference between returning body: messages and body: { messages }
-				body: messages
-			};
-		} else {
-			return {
-				status: 500
-			};
-		}
-	} catch (error) {
-		return {
-			status: 500,
-			body: {
-				error: 'Server error : ' + error
+	// https://github.com/prisma-labs/graphql-request#error-handling
+	const getMessages = gql`
+		query getMessages {
+			messages(first: 1000) {
+				id
+				name
+				text
+				price
 			}
+		}
+	`;
+	try {
+		const { messages } = await client.request(getMessages);
+		// console.log(JSON.stringify(messages, undefined, 2));
+		return {
+			status: 200,
+			body: messages
 		};
+	} catch (error) {
+		console.error(JSON.stringify(error, undefined, 2));
+		process.exit(1);
 	}
 }
