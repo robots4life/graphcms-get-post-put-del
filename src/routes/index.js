419b690f57307f8435ac4133c0a6d2f89a7fdce5
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-18 16:17:50 +0200

    cleanup

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,20 +5,20 @@ const GRAPH_CMS_MESSAGE_TOKEN = process.env['GRAPH_CMS_MESSAGE_TOKEN'];
 const requestHeaders = { authorization: 'Bearer ' + GRAPH_CMS_MESSAGE_TOKEN };
 
 export async function get() {
-	try {
-		const getMessages = gql`
-			query getMessages {
-				messages(first: 1000) {
-					id
-					name
-					text
-					price
-				}
+	const getMessages = gql`
+		query getMessages {
+			messages(first: 1000) {
+				id
+				name
+				text
+				price
 			}
-		`;
+		}
+	`;
+	// https://github.com/prisma-labs/graphql-request#error-handling
+	try {
 		const messages = await client.request(getMessages);
 		// console.log(JSON.stringify(messages, undefined, 2));
-
 		return {
 			status: 200,
 			body: messages
@@ -35,7 +35,6 @@ export async function post() {
 		text: '24',
 		price: Date.now()
 	};
-
 	const createMessage = gql`
 		mutation createMessage($name: String!, $text: String!, $price: Int!) {
 			createMessage(data: { name: $name, text: $text, price: $price }) {
@@ -46,7 +45,6 @@ export async function post() {
 			}
 		}
 	`;
-
 	const publishMessage = gql`
 		mutation publishMessage($id: ID!) {
 			publishMessage(where: { id: $id }, to: PUBLISHED) {
@@ -54,13 +52,10 @@ export async function post() {
 			}
 		}
 	`;
-
 	// https://github.com/prisma-labs/graphql-request#error-handling
-
 	try {
 		const createdMessage = await client.request(createMessage, variables, requestHeaders);
 		// console.log(JSON.stringify(createdMessage, undefined, 2));
-
 		try {
 			const messageID = { id: createdMessage.createMessage.id };
 			const publishedMessage = await client.request(
@@ -69,7 +64,7 @@ export async function post() {
 				requestHeaders
 			);
 			let returnedMessage = JSON.stringify(publishedMessage);
-			// console.log(returnedMessage);
+			console.log(returnedMessage);
 			return {
 				status: 200
 			};
